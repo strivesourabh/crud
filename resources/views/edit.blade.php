b46cba174ee8dcb9822efd9953d5de53e20c07dc
Your Name <you@example.com>
2022-07-20 18:31:36 +0530

    change

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+
+
+@foreach ($cruds as $item)
+    {{ $item->name }}
+    {{ $item->email }}
+    {{ $item->contact }}
+    {{ $item->city }}
+    {{ $item->country }}
+@endforeach
+
+
+
+@endsection
