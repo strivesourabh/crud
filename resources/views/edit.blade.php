bd3873503201dc16ebdbf6f2323e79f308fd8d63
Your Name <you@example.com>
2022-07-21 19:36:04 +0530

    edit view part

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 @section('content')
 @foreach ($cruds as $user)
+
 <form class="form" action="{{ url('edit', $user->id) }}" method="post" enctype="multipart/form-data">
     @csrf
-    
-    <input type="text" name="name" id="name" value=" {{ $user->name }}"><br>
-    <input type="text" name="email" id="email"  value=" {{ $user->email }}"><br>
-    <input type="text" name="contact" id="contact" value=" {{ $user->contact }}"><br>
-    <input type="text" name="city" id="city" value=" {{ $user->city }}"><br>
-    <input type="text" name="country" id="country" value=" {{ $user->country }}"><br>
-    <input type="file" name="file" id="file">
+    <div class="row-4">
+        <input type="text" name="name" id="name" value=" {{ $user->name }}">
+    </div>
+    <div class="row-4">
+        <input type="text" name="email" id="email"  value=" {{ $user->email }}"><br>
+    </div>
+    <div class="row-4">
+        <input type="text" name="contact" id="contact" value=" {{ $user->contact }}"><br>
+    </div>
+    <div class="row-4">
+        <input type="text" name="city" id="city" value=" {{ $user->city }}"><br>
+    </div>
+    <div class="row-4">
+        <input type="text" name="country" id="country" value=" {{ $user->country }}"><br>
+    </div>
+    <div class="row-4">
+        <input type="file" name="file" id="file">
     <td><img width="200" src="{{asset("upload/".$user->filename)}}" alt="as"></td><br>
-
+    </div>
     <input type="submit" value="Update">
 </form>
 @endforeach
--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.app')
 @section('content')
-
-
 @foreach ($cruds as $user)
-    {{ $user->name }} <br>
-    {{ $user->email }} <br>
-    {{ $user->contact }} <br>
-    {{ $user->city }} <br>
-    {{ $user->country }} <br>
-    <td><img width="200" src="{{asset("upload/".$user->filename)}}" alt="as"></td>
- 
+<div class="card mx-auto mt-5 text-left shadow-none p-3 mb-5 bg-light rounded" style="width: 18rem;">
+    <img src="{{asset("upload/".$user->filename)}}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Name: {{ $user->name }} </h5>
+      <p class="card-text">Email: {{ $user->email }}</p>
+      <p class="card-text">Contact: {{ $user->contact }}</p>
+      <p class="card-text">City: {{ $user->city }}</p>
+      <p class="card-text">Country: {{ $user->country }}</p>
+      <a href="/home" class="btn btn-primary">Go @ Home</a>
+    </div>
+  </div>
 @endforeach
 
 
